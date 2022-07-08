3613ad3338767453a171a8530c56f95bc9465f5f
Noelia Donato <noeliadonato@gmail.com>
2022-07-08 19:32:51 -0300

    fix: pre-commit, run test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 	return (
 		<main>
 			<h1 className='text-center text-3xl font-bold underline underline-offset-8 decoration-pink-500'>
-				Hello world 🌍!
+				Hello world!
 			</h1>
 		</main>
 	)
